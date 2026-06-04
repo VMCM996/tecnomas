47b1aeffd6b81392206468cbbf6f0d6831433d45
VMCM996 <vicmanuelch@gmail.com>
2026-06-04 14:18:30 -0400

    Agregar imagenes a los productos pt4

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -3,36 +3,51 @@
 export const listaEquipos = [
   { 
     id: 1, 
-    name: 'ASUS VIVO BOOK 15', 
+    name: 'LAPTOP ASUS VIVO BOOK 15', 
     specs: 'AMD RYZEN 7 7520U | 8GB DDR5 S-DIMM 15.6" FHD (1920X1080) | WIFI 6 BLUETOOTH 5.3', 
     price: 520, 
     img: '/images/ASUS-RYZEN5.png' 
   },
   { 
     id: 2, 
-    name: 'COMPAQ Q-BOOK', 
+    name: 'LAPTOP COMPAQ Q-BOOK', 
     specs: 'INTEL CORE i5-1235U | 8GB DDR4 15.6" FHD (1920X1080) | WIFI 5 BLUETOOTH 5.0', 
     price: 530, 
     img: '/images/I5-1235U.png' 
   },
   { 
     id: 3, 
-    name: 'COMPAQ Q-BOOK', 
+    name: 'LAPTOP COMPAQ Q-BOOK', 
     specs: 'INTEL CORE i3-1215U | 8GB DDR4 15.6" FHD (1920X1080) | WIFI 5 BLUETOOTH 5.0', 
     price: 480, 
     img: '/images/I3-1235U.png' 
   },
   { 
     id: 4, 
-    name: 'DELL LATITUDE 5400 (REFURBISHED)', 
-    specs: 'INTEL CORE i5-8365U | 8GB DDR4 14" FHD | WIFI 5 BLUETOOTH 5.0', 
+    name: 'LAPTOP DELL LATITUDE 5400 (REFURBISHED)', 
+    specs: 'INTEL CORE i5-8365U | 8GB DDR4 14" FHD TACTIL | WIFI 5 BLUETOOTH 5.0', 
     price: 250, 
     img: "/images/I5-5400.png" 
-  },{ 
+  },
+  { 
     id: 5, 
-    name: 'DELL LATITUDE 5400 (REFURBISHED)', 
-    specs: 'INTEL CORE i7-8665U | 8GB DDR4 14" FHD | WIFI 5 BLUETOOTH 5.0', 
+    name: 'LAPTOP DELL LATITUDE 5400 (REFURBISHED)', 
+    specs: 'INTEL CORE i7-8665U | 16GB DDR4 14" FHD | WIFI 5 BLUETOOTH 5.0', 
     price: 300, 
     img: "/images/I5-5400.png"
-  }
+  },
+  { 
+    id: 6, 
+    name: 'COMBO LAPTOP i5 + BOLSO', 
+    specs: 'INTEL CORE i5-8365U | 8GB DDR4 14" FHD TACTIL | WIFI 5 BLUETOOTH 5.0 Bolso HP Original 15.6"', 
+    price: 280, 
+    img: "/images/COMBOI7.png"
+  },
+  { 
+    id: 7, 
+    name: 'COMBO LAPTOP i7 + BOLSO', 
+    specs: 'INTEL CORE i5-8665U | 16GB DDR4 14" FHD | WIFI 5 BLUETOOTH 5.0 Bolso HP Original 15.6"', 
+    price: 330, 
+    img: "/images/COMBOI7.png"
+  },
 ];
